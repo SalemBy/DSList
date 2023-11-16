58e34d8bd013b3a450602d0c07e63ddffcda9dd9
Salem <juliocesarspq@gmail.com>
2023-11-16 14:50:35 -0300

    Create properties, game entitie and orm

---
+++ src/test/java/com/bysalem/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bysalem.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
